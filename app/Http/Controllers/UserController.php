29ebd79df12287f3bf9115e87f17d3b303db4f11
Femas Akbar Faturrohim <femasakbar69@gmail.com>
2026-03-01 19:20:11 +0700

    J3 : Praktikum-6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function index()
+    {
+        $data = [
+            'nama' => 'Pelanggan pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data);
+
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+
+
     public function show($id, $name)
     {
         return view('user', ['id' => $id, 'name' => $name]);
