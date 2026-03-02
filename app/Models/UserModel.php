8de34a20f0dd0e2d412a686b039c97b374dbde83
Femas Akbar Faturrohim <femasakbar69@gmail.com>
2026-03-02 19:52:40 +0700

    J4 : Praktikum-2.7

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserModel extends Model
 {
@@ -15,7 +16,12 @@ class UserModel extends Model
     protected $fillable = [
         'username',
         'nama',
-        'level_id',
+    'level_id',
         'password'
     ];
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
